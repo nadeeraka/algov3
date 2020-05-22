d9077c4195956e0085031962c97ef6ce754aec58
nadeeraka <nadeerakan@gmail.com>
2020-05-23 00:09:55 +0530

    exploring python

--- algov3/bin/s1/1.py
+++ algov3/bin/s1/1.py
@@ -1,9 +1,13 @@
 list1 = [1, 2, 3, 4, 1, 2, 3]
 
+
 # x = [1, 2, 3]
 # x.append([4, 5])
 # gives you: [1, 2, 3, [4, 5]]
 
-def sumOfDif (a):
-  return list(map(lambda x: x*2,a ))
+def sumOfDif(a):
+    return list(map(lambda x: x * 2, a))
 
+
+def filterOdd(a):
+    return list(filter(lambda x: x / 2 == 1, a))
